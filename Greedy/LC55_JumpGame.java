28b03a95954d6245b758692f9313df2988a529db
lavanya agrawal <lavanya.kanak.agrawal@gmail.com>
2026-06-19 13:16:31 +0530

    Add solution for Jump Game problem
    Implement solution for LeetCode problem 55, Jump Game.

--- Greedy/LC55_JumpGame.java
+++ Greedy/LC55_JumpGame.java
@@ -1,1 +1,24 @@
+/*
+LeetCode #55 - Jump Game
+Difficulty: Medium
+Topic: Greedy
+*/
+
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int maxReach = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+
+            if(i > maxReach) {
+                return false;
+            }
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
 
