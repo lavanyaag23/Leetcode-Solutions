2a2eb216dc6d5afedd419a6e53bdf257c9c77ed0
lavanya agrawal <lavanya.kanak.agrawal@gmail.com>
2026-06-19 13:21:39 +0530

    Update LC66_PlusOne.java

--- Arrays/LC66_PlusOne.java
+++ Arrays/LC66_PlusOne.java
@@ -1,1 +1,32 @@
+/*
+ * LeetCode Problem #66 - Plus One
+ *
+ * Difficulty: Easy
+ * Topic: Arrays
+ *
+ * Problem Link:
+ * https://leetcode.com/problems/plus-one/
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
 
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+} 
