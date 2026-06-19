2986e578cfb1f80f8fe6564483e4fec6e4e1f5a0
lavanya agrawal <lavanya.kanak.agrawal@gmail.com>
2026-06-19 13:22:19 +0530

    Update LC867_TransposeMatrix.java

--- Arrays/LC867_TransposeMatrix.java
+++ Arrays/LC867_TransposeMatrix.java
@@ -1,1 +1,29 @@
-Add LeetCode #867 Transpose Matrix solution
+/*
+ * LeetCode Problem #867 - Transpose Matrix
+ *
+ * Difficulty: Easy
+ * Topic: Arrays, Matrix
+ *
+ * Problem Link:
+ * https://leetcode.com/problems/transpose-matrix/
+ *
+ * Time Complexity: O(m × n)
+ * Space Complexity: O(m × n)
+ */
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] result = new int[cols][rows];
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+}
