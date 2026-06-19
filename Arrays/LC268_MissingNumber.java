3450795f45615d8d73b4d8c8096b17b2c6516fc4
lavanya agrawal <lavanya.kanak.agrawal@gmail.com>
2026-06-19 13:12:14 +0530

    Add LeetCode #268 Missing Number solution

---
+++ Arrays/LC268_MissingNumber.java
@@ -0,0 +1,18 @@
+/*
+LeetCode #268 - Missing Number
+Difficulty: Easy
+Topic: Arrays
+*/
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+
+        for (int num : nums) {
+            sum -= num;
+        }
+
+        return sum;
+    }
+}
