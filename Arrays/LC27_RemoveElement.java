f0b1a107728e2339c38002e25ec6d26c4fe35cc8
lavanya agrawal <lavanya.kanak.agrawal@gmail.com>
2026-06-19 13:20:47 +0530

    Update LC27_RemoveElement.java

--- Arrays/LC27_RemoveElement.java
+++ Arrays/LC27_RemoveElement.java
@@ -1,1 +1,29 @@
+/*
+ * LeetCode Problem #27 - Remove Element
+ *
+ * Difficulty: Easy
+ * Topic: Arrays
+ *
+ * Problem Link:
+ * https://leetcode.com/problems/remove-element/
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
 
