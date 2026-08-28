36b2215cda0b572b029d2e39217477dcd76aeb2f
lavanya agrawal <lavanya.kanak.agrawal@gmail.com>
2026-08-28 10:33:06 +0530

    Create LC877_StoneGame.java

---
+++ DynamicProgramming/LC877_StoneGame.java
@@ -0,0 +1,26 @@
+/*
+ * LeetCode Problem #877 - Stone Game
+ *
+ * Difficulty: Medium
+ * Topic: Dynamic Programming, Game Theory
+ *
+ * Problem Link:
+ * https://leetcode.com/problems/stone-game/
+ *
+ * Approach:
+ * Alice and Bob play optimally. Since the number of piles is even,
+ * Alice can always guarantee a win by choosing the stones from either
+ * all even-indexed or all odd-indexed piles.
+ *
+ * Therefore, Alice always wins.
+ *
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
